9331784448dcb67b92c8cdce0ae9a2fd390d0566
pradyparanjpe <pradyparanjpe@rediffmail.com>
2020-10-17 21:10:29 +0530

    snatch_own is not modular with functions

--- snatch_own.sh
+++ snatch_own.sh
@@ -19,99 +19,109 @@
 #
 # Files in this project contain regular utilities and aliases for linux (Fc31)
 
-# Clear Cache
-
-# This must be run strictly as root
-
-
-
-# Variables
-
-ROBBER="$(logname)"
-SYNDICATE=
-RECURSE=false
-VERBOSE=false
-USAGE_MESSAGE="$0
-Snatch Ownership of Files
+function set_var() {
+    # Variables
+    ROBBER="$(logname)"
+    SYNDICATE=
+    RECURSE=false
+    VERBOSE=false
+    USAGE_MESSAGE="$0:
+Snatch Ownership
 
 usage:
-$0 [-h|--help] [-R] [-v] [-u|--user ROBBER] TARGET
+$0 [-h|--help] [-R] [-v] [-u|--user ROBBER] [-g|--group SYNDICATE] TARGET
 
 Optional Arguments
 -h|--help\t\tPrint this message and exit
 -R\t\t\tApply recursive
 -v\t\t\tVerbose outout
 -u|--user ROBBER\tGive ownership to ROBBER [default: ${ROBBER}]
--g|--group SYNDICATE\tGroup ownership [default: ROBBER's default]
+-g|--group SYNDICATE\tGroup ownership [default: ROBBER's primary]
 
 Positional Argument
 
 TARGET
 "
+}
 
-[[ $# -eq 0 ]] && echo -e "$USAGE_MESSAGE" && exit 0
+function unset_var () {
+    ROBBER=
+    VERBOSE=
+    RECURSE=
+    SYNDICATE=
+    CLI=
+}
 
-while test $# -ge 1; do
-    case $1 in
-        -h|--help)
-            echo -e "${USAGE_MESSAGE}"
-            exit 0;
-            ;;
-        -R)
-            RECURSE=true
-            ${VERBOSE} && echo "[VERBOSE] SET RECURSIVE"
-            shift 1
-            ;;
-        -v)
-            VERBOSE=true
-            ${VERBOSE} && echo "[VERBOSE] SET VERBOSE"
-            shift 1
-            ;;
-        -u|--user)
-            shift 1
-            ROBBER="$1"
-            ${VERBOSE} && echo "[VERBOSE] SET ROBBER $ROBBER"
-            shift 1
-            ;;
-        -g|--group)
-            shift 1
-            SYNDICATE="$1"
-            ${VERBOSE} && echo "[VERBOSE] SET SYNDICATE $SYNDICATE"
-            shift 1
-            ;;
-        *)
-            TARGET="${TARGET}${1}"
-            shift 1
-            ;;
-    esac
-done
-
-[[ -z "${TARGET}" ]] && echo -e "${USAGE_MESSAGE}" && exit 0
-
-[[ ! -e "${TARGET}" ]] \
-    && echo -e "  [ERROR] Confirm that ${TARGET} exists." >&2 \
-    && exit 2
-
-if ! id -u "${ROBBER}" >> /dev/null; then
-    echo -e "[ERROR]   Confirm that the ${ROBBER} exists" >&2
-    exit 1
-fi
-
-[[ -z $SYNDICATE ]] && SYNDICATE="$(groups ${ROBBER} | cut -d ":" -f 2 \
+function get_cli() {
+    [[ $# -eq 0 ]] && echo -e "$USAGE_MESSAGE" && exit 0
+    while test $# -ge 1; do
+        case $1 in
+            -h|--help)
+                echo -e "${USAGE_MESSAGE}"
+                exit 0;
+                ;;
+            -R)
+                RECURSE=true
+                ${VERBOSE} && echo "[VERBOSE] SET RECURSIVE"
+                shift 1
+                ;;
+            -v)
+                VERBOSE=true
+                ${VERBOSE} && echo "[VERBOSE] SET VERBOSE"
+                shift 1
+                ;;
+            -u|--user)
+                shift 1
+                ROBBER="$1"
+                ${VERBOSE} && echo "[VERBOSE] SET ROBBER $ROBBER"
+                shift 1
+                ;;
+            -g|--group)
+                shift 1
+                SYNDICATE="$1"
+                ${VERBOSE} && echo "[VERBOSE] SET SYNDICATE $SYNDICATE"
+                shift 1
+                ;;
+            *)
+                TARGET="${TARGET}${1}"
+                shift 1
+                ;;
+        esac
+    done
+    [[ -z $SYNDICATE ]] && SYNDICATE="$(groups ${ROBBER} | cut -d ":" -f 2 \
 | cut -d " " -f 2)"
+}
+
+function sanity_check () {
+    [[ -z "${TARGET}" ]] && echo -e "${USAGE_MESSAGE}" && exit 0
+    [[ ! -e "${TARGET}" ]] \
+        && echo -e "  [ERROR] Confirm that ${TARGET} exists." >&2 \
+        && exit 2
+
+    if ! id -u "${ROBBER}" >> /dev/null; then
+        echo -e "[ERROR]   Confirm that the ${ROBBER} exists" >&2
+        exit 1
+    fi
+}
 
 
-CLI="chown ${ROBBER}:${SYNDICATE}"
-${RECURSE} && CLI="${CLI} -R"
-${VERBOSE} \
-    && CLI="${CLI} -v"\
-    && echo "[VERBOSE] Snatching ownership of ${TARGET} to\
+function prep_command () {
+    CLI="chown ${ROBBER}:${SYNDICATE}"
+    ${RECURSE} && CLI="${CLI} -R"
+    ${VERBOSE} \
+        && CLI="${CLI} -v"\
+        && echo "[VERBOSE] Snatching ownership of ${TARGET} to\
  ${ROBBER}:${SYNDICATE}"\
-    && echo -e "executing\n$CLI $TARGET"
-sudo su -c "${CLI} ${TARGET}"
+        && echo -e "executing\n$CLI $TARGET"
+}
 
-ROBBER=
-VERBOSE=
-RECURSE=
-SYNDICATE=
-CLI=
+function main () {
+    set_var
+    get_cli "$@"
+    sanity_check
+    prep_command
+    sudo su -c "${CLI} ${TARGET}"
+    unset_var
+}
+
+main "$@"
