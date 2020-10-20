f773d05beb65bd6ce89522f19079c7aa0c77fc54
pradyparanjpe <pradyparanjpe@rediffmail.com>
2020-10-20 13:18:42 +0530

    mvlns: a bash script to move an object and leave a link behind

--- host_on_net.sh
+++ host_on_net.sh
@@ -20,4 +20,9 @@
 # Files in this project contain regular utilities and aliases for linux (Fc31)
 
 # Bind port 80 to net
-nohup ssh -R $username:80:localhost:19999 -R $username:80:localhost:80 serveo.net 2>&1 >> ${HOME}/.server.log &
+nohup\
+	ssh\
+	-R $username:19999:localhost:19999\
+	serveo.net 2>&1 >> ${HOME}/.server.log\
+	&
+
